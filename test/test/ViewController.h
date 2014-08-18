6cfd284077328668b35ea08068d24e0d28ae38ca
huaisanwang <huaisanwang52033>
2014-08-18 15:41:05 +0800

    add new

--- test/test/ViewController.h
+++ test/test/ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////
 @end
--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -18,6 +18,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
 }
 
 - (void)didReceiveMemoryWarning
