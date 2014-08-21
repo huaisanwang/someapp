212d8d2e3c0499685d1f41848eda8f6cc8da26d2
huaisan.wang <632494788@qq.com>
2014-08-21 23:30:49 +0800

    添加新控制器

---
+++ test/test/GitTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  GitTestViewController.h
+//  test
+//
+//  Created by apple on 14-8-21.
+//  Copyright (c) 2014年 Huaisan.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitTestViewController : UIViewController
+
+@end
